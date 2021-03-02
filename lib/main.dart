b3db04124abd9323f3d474f2be67bd3f070d46c6
shunsukenoguchi <bakutensanba89@yahoo.ne.jp>
2021-03-02 21:28:37 +0900

    chenge text

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'test',
+              'chenge',
             ),
           ],
         ),
